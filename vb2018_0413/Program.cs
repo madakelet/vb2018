95a6298e379f4ecd72ac59bf15bf6757d7ee6e62
KeletMada <mileaddy@gmail.com>
2021-04-13 23:32:31 +0200

    beolvasas

--- vb2018_0413/Program.cs
+++ vb2018_0413/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace vb2018_0413
     {
         static void Main(string[] args)
         {
+            //2
+
+            string[] sorok = File.ReadAllLines("vb2018.txt");
+            List<Stadion> stadionok = new List<Stadion>();
+            foreach  (string sor in sorok.Skip(1))
+            {
+                stadionok.Add(new Stadion(sor));
+            }
+
+
         }
     }
 }
---
+++ vb2018_0413/Stadion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vb2018_0413
+{
+    class Stadion
+    {
+        public string Varos { get;  }
+        public string StadionNev { get;  }
+        public string AlternativNev { get; }
+        public int Ferohely { get; }
+
+        public Stadion(string sor)
+        {
+            string[] s = sor.Split(';');
+            Varos = s[0];
+            StadionNev = s[1];
+            AlternativNev = s[2];
+            Ferohely = int.Parse(s[3]);
+        }
+    }
+}
